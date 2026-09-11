184bca64a04d25629c04a0d080ecd387b3165bd9
Chaitanya  Pawar <pawarchaitanya952@gmail.com>
2026-09-11 21:56:11 +0530

    Update API endpoint to relative path

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,7 +17,7 @@ function Contact() {
 
     try {
       // Send data to Node.js backend
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
